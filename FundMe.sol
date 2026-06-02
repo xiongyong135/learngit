24c723308dcb61b17f95b51f369ad63b85541fa5
paymon <paymon@safexapp.com>
2026-06-02 16:57:33 +0800

    8

--- FundMe.sol
+++ FundMe.sol
@@ -3,7 +3,10 @@ pragma solidity ^0.8.19;
 
 import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
 
-
+// 1.创建一个收款函数
+// 2.记录投资人并查看
+// 3.在锁定期内，达到目标值，生产商可以提款
+// 4.在锁定期内，没有达到目标值，投资人在锁定期以后退款
 contract FundMe{
     mapping (address => uint) public funderToAmount;
     // uint MIN_AMOUNT = 1 * 10 ** 18;  // 1个eth = 10 ** 18 wei
@@ -14,16 +17,22 @@ contract FundMe{
 
     address public  owner;
 
-    constructor () {
+    uint deployTime;
+    uint lockTime;
+
+    constructor (uint _lockTime) {
         // sepolia testnet
         priceFeed = AggregatorV3Interface(0x694AA1769357215DE4FAC081bf1f309aDC325306);
         owner = msg.sender;
+        deployTime = block.timestamp;
+        lockTime = _lockTime;
     }
 
     function fund() external payable {
         uint amount = msg.value;
         uint usdAmount = convertEth2USD(amount);
         require(usdAmount >= MIN_AMOUNT, "USD Amount too low!");
+        require(block.timestamp < deployTime + lockTime, "window is closed!");
         // require(amount >= MIN_AMOUNT, "Amount too low!");
 
         funderToAmount[msg.sender] += msg.value;
@@ -59,12 +68,14 @@ contract FundMe{
         return ethAmount * price / (10 ** 8);
     }
 
-    // 提款，金额满足，owner可以提款将合约余额转到用户余额
+    // 提款，锁定期满，金额满足，owner可以提款将合约余额转到用户余额
     function getFund() external {
         uint contractBalance = address(this).balance;
         address sender = msg.sender;
         require(convertEth2USD(contractBalance) >= TARGET_AMOUNT, "Not Reach Target Amount!");
         require(owner == sender, "you are not the owner!");
+        require(block.timestamp >= deployTime + lockTime, "window is closed!");
+
         // transfer
         // payable (sender).transfer(contractBalance);
         // send
@@ -74,19 +85,22 @@ contract FundMe{
         bool success;
         (success, ) = payable (sender).call{value: contractBalance}("");
         require(success, "Failed!");
-
+        funderToAmount[sender] = 0;
     }
 
-    // 退款，如果目标未达成，则投资用户可以提走自己的投资金额
+    // 退款，锁定期满，如果目标未达成，则投资用户可以提走自己的投资金额
     function refund() external {
         address myAddress = msg.sender;
         uint contractBalance = address(this).balance;
         require(contractBalance < TARGET_AMOUNT, "Target is reached!");
         uint myContractAmount = funderToAmount[myAddress];
         require(myContractAmount > 0, "you have no balance in this contract!");
+        require(block.timestamp >= deployTime + lockTime, "window is closed!");
+
         bool success;
         (success, ) = payable (myAddress).call{value: myContractAmount}("");
         require(success, "Failed!");
+        funderToAmount[myAddress] = 0;
     }
 
 }
